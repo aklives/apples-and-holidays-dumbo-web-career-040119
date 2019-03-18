2a72b7f63c52c26049076426a74fb24db3ec7944
Adam Kearney <aklives@hotmail.com>
2019-03-18 04:32:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,5 +79,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_hash.each do |k, v|
+   holiday_hash[k].each do |k, v|
+     if v.include? "BBQ"
+       k 
+     end
+   end
+ end
 
 end
